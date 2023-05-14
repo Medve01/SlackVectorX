df7445f7c78e969af359bf34bbc6aa786bd850ba
Bear <csaba.ferenclaszlo@zapier.com>
2023-05-14 11:01:24 +0200

    dotenv and dockerize

--- slackvectorx/config.py
+++ slackvectorx/config.py
@@ -1,13 +1,22 @@
 import os
 import json
+import dotenv
 
 class Config:
     def __init__(self, filename="config.json"):
-        data_dir = os.getenv("DATA_DIR", "/data")
+        data_dir = dotenv.get_key('.env', 'DATA_DIR')
+        openai_api_key = dotenv.get_key('.env', 'OPENAI_API_KEY')
+        slack_bot_token = dotenv.get_key('.env', 'SLACK_BOT_TOKEN')
+        slack_app_token = dotenv.get_key('.env', 'SLACK_APP_TOKEN')
+        slack_signing_secret = dotenv.get_key('.env', 'SLACK_SIGNING_SECRET')
         try:
             with open(filename) as f:
                 self.config = json.load(f)
                 self.config["DATA_DIR"] = data_dir
+                self.config["OPENAI_API_KEY"] = openai_api_key
+                self.config["SLACK_BOT_TOKEN"] = slack_bot_token
+                self.config["SLACK_APP_TOKEN"] = slack_app_token
+                self.config["SLACK_SIGNING_SECRET"] = slack_signing_secret
         except FileNotFoundError:
             print("Error: config.json not found")
             self.config = {}
